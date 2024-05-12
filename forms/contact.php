404d896f89d24695162bbd707d0d931a518a614e
GvtechFix <140695207+GvtechFix@users.noreply.github.com>
2024-05-12 23:18:56 +0530

    Update contact.php

--- forms/contact.php
+++ forms/contact.php
@@ -1,11 +1,12 @@
 <?php
-// Replace gvtechfix@gmail.com with your real receiving email address
-$receiving_email_address = 'gvtechfix@gmail.com';
-
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Replace this email address with your own receiving email address
+    $to = "gvtechfix@gmail.com";
+    $subject = "New Appointment Booking";
+
+    // Gather form data
     $name = $_POST['name'];
     $email = $_POST['email'];
-    $subject = "New Appointment Booking";
     $mobile = $_POST['mobile'];
     $brandname = $_POST['brandname'];
     $model = $_POST['model'];
@@ -13,7 +14,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $warranty = $_POST['warranty'];
     $description = $_POST['message'];
 
-    // Construct message
+    // Compose the email message
     $message = "Name: $name\n";
     $message .= "Email: $email\n";
     $message .= "Mobile: $mobile\n";
@@ -23,8 +24,11 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $message .= "Warranty: $warranty\n";
     $message .= "Description:\n$description";
 
+    // Set additional headers
+    $headers = "From: $name <$email>";
+
     // Send email
-    $mail_success = mail($receiving_email_address, $subject, $message);
+    $mail_success = mail($to, $subject, $message, $headers);
 
     if ($mail_success) {
         echo "SUCCESS";
